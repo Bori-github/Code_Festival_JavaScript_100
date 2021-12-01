2a8206291c3b90948ff8011907ad683bbe41c36e
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-01 14:05:46 +0900

    21/12/01 001-003

--- 001.js
+++ 001.js
@@ -1,6 +1,7 @@
 // 배열의 삭제
 let nums = [100, 200, 300, 400, 500];
 
+// slice로 배열의 0번째부터 (3-1)번째 까지 추출
 let deleteNums = nums.slice(0, 3);
 
 console.log(deleteNums);
--- 002.js
+++ 002.js
@@ -1,9 +1,9 @@
 // 배열의 내장함수
 let arr = [200, 100, 300];
 
-let arr1 = arr.slice(0, 2);
-let arr2 = arr.slice(2);
+let arr1 = arr.slice(0, 2); // [200, 100]
+let arr2 = arr.slice(2); // [300]
 
-arr = [...arr1, 1000, ...arr2];
+arr = [...arr1, 1000, ...arr2]; // 전개 구문을 이용해서 배열 합치기
 
 console.log(arr);
