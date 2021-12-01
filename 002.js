65c38c22b6b08f73dae363ab1023bba447ddcee8
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-01 09:40:02 +0900

    21/12/01 001-003

---
+++ 002.js
@@ -0,0 +1,9 @@
+// 배열의 내장함수
+let arr = [200, 100, 300];
+//pass
+let arr1 = arr.slice(0, 2);
+let arr2 = arr.slice(2);
+
+arr = [...arr1, 1000, ...arr2];
+
+console.log(arr);
---
+++ 003.js
@@ -0,0 +1,4 @@
+// 변수의 타입
+var arr = [100, 200, 300];
+
+console.log(typeof arr);
