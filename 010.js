b08782755d3beaac6f09d95de59b2ad057e121b5
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-07 01:01:34 +0900

    21/12/06 010-012

--- 010.js
+++ 010.js
@@ -1,6 +1,9 @@
 let star = "";
 for (let i = 0; i < 5; i++) {
-  for (let j = 0; j <= i; j++) {
+  for (let j = 4; j > i; j--) {
+    star += " ";
+  }
+  for (let k = 0; k <= i * 2; k++) {
     star += "*";
   }
   star += "\n";
